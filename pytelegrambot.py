0037ff8ed72f4bd3308d3f87bb947fdb1bf22efa
dieguito84 <diego.mariani84@gmail.com>
2019-04-13 02:04:33 +0200

    added bytecode to .gitignore and further tests

--- pytelegrambot.py
+++ pytelegrambot.py
@@ -1,10 +1,15 @@
 import config
 import requests
 
-get_me = config.common_api + "getMe"
-get_updates = config.common_api + "getUpdates"
 
-get_chat = config.common_api + "getChat?" + config.chat_id
+hw = "Hello world!"    # da cancellare
+
+get_me = config.common_api + "getMe"    # da cancellare
+get_updates = config.common_api + "getUpdates"    # creare funzione
+
+get_chat = config.common_api + "getChat?" + config.chat_id    # da cancellare
+
+send_message = config.common_api + "SendMessage?" + config.chat_id + "&text=" + hw    # creare funzione
 
 
 # req = requests.get(get_me)
@@ -19,6 +24,12 @@ get_chat = config.common_api + "getChat?" + config.chat_id
 
 req2 = requests.get(get_updates)
 # print(req2.json())
-print(req2.json()["result"][len(req2.json()["result"])-1]["message"]["text"])
-# print(len(req2.json()["result"][len(req2.json()["result"])-1]["message"]["entities"])-1)
-print(req2.json()["result"][len(req2.json()["result"])-1]["message"]["entities"][len(req2.json()["result"][len(req2.json()["result"])-1]["message"]["entities"])-1]["type"])
+message = req2.json()["result"][len(req2.json()["result"])-1]["message"]["text"]    # da sistemare
+try:
+    if "bot_command" in req2.json()["result"][len(req2.json()["result"])-1]["message"]["entities"][len(req2.json()["result"][len(req2.json()["result"])-1]["message"]["entities"])-1]["type"]:    # da sistemare
+        is_bot_command = True
+except:
+    pass
+
+if "/helloworld" in message and is_bot_command:
+    req4 = requests.get(send_message)
